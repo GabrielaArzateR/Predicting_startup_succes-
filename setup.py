361ec7ccc95363fcbdfd50dd3444e4c119b37f9b
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-11-10 18:05:33 +0100

    setting up template

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ def read(fname: str) -> str:
 setup(
     name="Dummy module",
     version="0.0.1",
-    author="Axel Mendoza",
-    author_email="axel.mendoza@epita.fr",
+    author="Gabriela Arzate",
+    author_email="lesliegabrielardz@outlook.com",
     description="Example description",
     license="BSD",
-    packages=['mymodule'],
+    packages=['startup_succes '],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
