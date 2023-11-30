c493707a16db5f0fe74bcdc4312ee90ca6e6a67e
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-11-30 07:36:52 +0100

    adding test files

---
+++ mymodule/tests/test_helloword.py
@@ -0,0 +1,10 @@
+# test_helloword.py
+
+"""Dummy module description"""
+from mymodule.helloword import helloword
+
+
+def test_helloworld() -> None:
+    """Tests the hello world function"""
+    helloword()
+    assert True
