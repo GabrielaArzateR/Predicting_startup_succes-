28d53b7d3cce363f1641799dfa8d8c4c12c0df3a
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-12-01 08:20:36 +0100

    test changes

--- startup_prediction/tests/test_helloword.py
+++ startup_prediction/tests/test_helloword.py
@@ -1,20 +1,20 @@
-"""
-Dummy module description
-"""
+# """
+# Dummy module description
+# """
 
-from startup_prediction.script import StartupPredictor
-
-
-# pylint: disable=R0903
-class TestStartupPredictor:
-    """Tests the StartupPredictor class"""
-
-    def test_startup_predictor(self) -> None:
-        """Test the StartupPredictor class with a specified data path"""
-        data_path = "data/startup.csv"
-        StartupPredictor(data_path)
+# from startup_prediction.script import StartupPredictor
 
 
-if __name__ == '__main__':
-    test_instance = TestStartupPredictor()
-    test_instance.test_startup_predictor()
+# # pylint: disable=R0903
+# class TestStartupPredictor:
+#     """Tests the StartupPredictor class"""
+
+#     def test_startup_predictor(self) -> None:
+#         """Test the StartupPredictor class with a specified data path"""
+#         data_path = "data/startup.csv"
+#         StartupPredictor(data_path)
+
+
+# if __name__ == '__main__':
+#     test_instance = TestStartupPredictor()
+#     test_instance.test_startup_predictor()
